a02ebad76f9c9378cab829dd5e2eccde188e673d
aku ibnnu <ibnudev@medeva.tech>
2025-02-19 21:17:08 +0700

    feat: Add data table component for payment management with sorting and filtering

--- app/(protected)/kasir/page.tsx
+++ app/(protected)/kasir/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Page = () => {
-  return (
-    <div>Halaman Kasir</div>
-  )
+// Datatabel component
+import { DataTable } from "./_datatable/data-table";
+import { columns, Payment } from "./_datatable/columns";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
 }
 
-export default Page
+const Page = async () => {
+  const data = await getData();
+  return (
+    <>
+      <DataTable columns={columns} data={data} />
+    </>
+  );
+};
+
+export default Page;
